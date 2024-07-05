c891b27282dfde23c71553a751b5fbc34c65c60e
zakariyasalie <zakariya.salie07@gmail.com>
2024-07-05 14:48:35 +0200

    update

--- script/code.js
+++ script/code.js
@@ -1,11 +1,30 @@
-function convert(source) {
-    if (source === 'celsius') {
-        var celsius = parseFloat(document.getElementById('celsius').value);
-        var fahrenheit = (celsius * 9 / 5) + 32;
-        document.getElementById('fahrenheit').value = isNaN(fahrenheit) ? '' : fahrenheit.toFixed(2);
-    } else if (source === 'fahrenheit') {
-        var fahrenheit = parseFloat(document.getElementById('fahrenheit').value);
-        var celsius = (fahrenheit - 32) * 5 / 9;
-        document.getElementById('celsius').value = isNaN(celsius) ? '' : celsius.toFixed(2);
-    }
+// function convert(source) {
+//     if (source === 'celsius') {
+//         var celsius = parseFloat(document.getElementById('celsius').value);
+//         var fahrenheit = (celsius * 9 / 5) + 32;
+//         document.getElementById('fahrenheit').value = isNaN(fahrenheit) ? '' : fahrenheit.toFixed(2);
+//     } else if (source === 'fahrenheit') {
+//         var fahrenheit = parseFloat(document.getElementById('fahrenheit').value);
+//         var celsius = (fahrenheit - 32) * 5 / 9;
+//         document.getElementById('celsius').value = isNaN(celsius) ? '' : celsius.toFixed(2);
+//     }
+// }
+
+// function addition(numb 1 + numb) {
+//     console.log('My name is ${firstName} ${lastName}');
+// }
+// // Arguements
+// greeting('Zak'+ 'Salie')
+// greeting('Azhar' + 'Jacobs')
+
+function addition(numb1 , numb2) {
+    console.log(numb1 + numb2)
 }
+addition(Zak, Salie)
+
+
+
+
+
+
+
